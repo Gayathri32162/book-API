84d88f487c00e7cd6a18022316b187c2cd9c8243
Gayathri32162 <gayathribalamurugan82@gmail.com>
2021-08-17 12:25:45 +0530

    updated

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require("express");  //installing express
 
+//database
+const Database = require("./database")
+
 const ourApp = express();//initializing ourApp with express features
 
 //creating server
 
-ourApp.get("/", (request,response) => { //route
-        response.json({message: "Request served!!!!!!!!"});
+ourApp.get("/", (request,response) => {    //route
+        response.json({message: "server is working"});    //test api
+});
+
+ourApp.get("/book", (req,res) => {
+    return res.json({ books: Database.Book});
 });
 
 ourApp.listen(4000, () => console.log("server is running"));
